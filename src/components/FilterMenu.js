3469cd6e07a9582b36c4c5a55bf0372036f742a6
Amber Kim <ambergkim@gmail.com>
2019-11-09 11:45:18 -0800

    added filter menu and list accordion

---
+++ src/components/FilterMenu.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  CheckBox
+} from 'react-native';
+
+const FilterMenu: () => React$Node = () => {
+  return (
+    <>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Filter Candidate Results</Text>
+        <View style={styles.filterItem}>
+          <CheckBox />
+          <Text style={styles.filterText}>Software Developer / Engineer</Text>
+        </View>
+        <View style={styles.filterItem}>
+          <CheckBox />
+          <Text style={styles.filterText}>Looking For Part-Time Work</Text>
+        </View>
+        <View style={styles.filterItem}>
+          <CheckBox />
+          <Text style={styles.filterText}>Technical Profile</Text>
+        </View>
+        <View style={styles.filterItem}>
+          <CheckBox />
+          <Text style={styles.filterText}>Non-Technical Profile</Text>
+        </View>
+        <View style={styles.filterItem}>
+          <CheckBox />
+          <Text style={styles.filterText}>Washington State</Text>
+        </View>
+
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  filterItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  filterText: {
+    fontSize: 18
+  },
+  avatar: {
+    width: 50, height: 50, borderRadius: 50
+  },
+  sectionContainer: {
+    marginTop: 32,
+    flexDirection: "column",
+  },
+  sectionTitle: {
+    marginLeft: 10,
+    fontSize: 20,
+  }
+});
+
+export default FilterMenu;
